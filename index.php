b68dfe259203db3da6b32e09a2d1305dd9c02baf
doannvph33201 <118094808+doannvph33201@users.noreply.github.com>
2023-11-11 19:18:11 +0700

    bo deo thic?

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include "view/layout/header.php";
+include "view/layout/banner.php";
+include "view/public/ticketsearch.php";
+include "view/public/home.php";
+include "view/layout/footer.php";
+
+//include "view/public/404.php";
---
+++ view/layout/header.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css?ver=<?= rand() ?>">
+    <link rel="stylesheet" href="assets/css/all.min.css?ver=<?= rand() ?>">
+    <link rel="stylesheet" href="assets/css/animate.css?ver=<?= rand() ?>">
+    <link rel="stylesheet" href="assets/css/flaticon.css?ver=<?= rand() ?>">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css?ver=<?= rand() ?>">
+    <link rel="stylesheet" href="assets/css/odometer.css?ver=<?= rand() ?>">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css?ver=<?= rand() ?>">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css?ver=<?= rand() ?>">
+    <link rel="stylesheet" href="assets/css/nice-select.css?ver=<?= rand() ?>">
+    <link rel="stylesheet" href="assets/css/jquery.animatedheadline.css?ver=<?= rand() ?>">
+    <link rel="stylesheet" href="assets/css/main.css?ver=<?= rand() ?>">
+
+    <link rel="shortcut icon" href="assets/images/favicon.png" type="image/x-icon">
+
+    <title>Boleto - Online Ticket Booking Website HTML Template</title>
+
+
+</head>
+
+<body>
+    <!-- ==========Preloader========== -->
+    <div class="preloader">
+        <div class="preloader-inner">
+            <div class="preloader-icon">
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+    </div>
+    <!-- ==========Preloader========== -->
+    <!-- ==========Overlay========== -->
+    <div class="overlay"></div>
+    <a href="#0" class="scrollToTop">
+        <i class="fas fa-angle-up"></i>
+    </a>
+    <!-- ==========Overlay========== -->
+
+    <!-- ==========Header-Section========== -->
+    <header class="header-section">
+        <div class="container">
+            <div class="header-wrapper">
+                <div class="logo">
+                    <a href="index.html">
+                        <img style="width: 170px; height: 80px; object-fit: cover;" src="assets/images/logo/logoquy.png" alt="logo">
+                    </a>
+                </div>
+                <ul class="menu">
+                    <li>
+                        <a href="#0" class="active">Home</a>
+                    </li>
+                    <li>
+                        <a href="#0">movies</a>
+                    </li>
+                    <li>
+                        <a href="contact.html">location</a>
+                    </li>
+                    <li class="header-button pr-0">
+                        <a href="sign-up.html">join us</a>
+                    </li>
+                </ul>
+                <div class="header-bar d-lg-none">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ==========Header-Section========== -->
