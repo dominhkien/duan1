eaffc4eb813c70cff89cda9e710f1abd71abeb8b
doannvph33201 <118094808+doannvph33201@users.noreply.github.com>
2023-11-12 09:21:18 +0700

    doan666

--- admin/phim/add.php
+++ admin/phim/add.php
@@ -1,46 +1,54 @@
-            <form class="form-horizontal">
+            <form action="index.php?act=addphim" class="form-horizontal" method="post" enctype="multipart/form-data">
                 <div class="card-body">
-                    <h4 class="card-title">Personal Info</h4>
+                    <h4 class="card-title text-center">Thêm phim</h4>
                     <div class="form-group row">
-                        <label for="fname" class="col-sm-3 text-right control-label col-form-label">First Name</label>
+                        <label class="col-sm-3 text-right control-label col-form-label">Tên phim</label>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control" id="fname" placeholder="First Name Here">
+                            <input name="ten_phim" type="text" class="form-control" placeholder="Tên phim">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="lname" class="col-sm-3 text-right control-label col-form-label">Last Name</label>
+                        <label class="col-sm-3 text-right control-label col-form-label">Ảnh</label>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control" id="lname" placeholder="Last Name Here">
+                            <input name="anh" type="file" class="form-control" placeholder="Ảnh">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="lname" class="col-sm-3 text-right control-label col-form-label">Password</label>
+                        <label class="col-sm-3 text-right control-label col-form-label">Mô tả</label>
                         <div class="col-sm-9">
-                            <input type="password" class="form-control" id="lname" placeholder="Password Here">
+                            <input name="mo_ta" type="Mô tả" class="form-control" placeholder="Mô tả">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="email1" class="col-sm-3 text-right control-label col-form-label">Company</label>
+                        <label class="col-sm-3 text-right control-label col-form-label">Thời
+                            lượng</label>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control" id="email1" placeholder="Company Name Here">
+                            <input name="thoi_luong" type="text" class="form-control" placeholder="Thời lượng">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="cono1" class="col-sm-3 text-right control-label col-form-label">Contact No</label>
+                        <label class="col-sm-3 text-right control-label col-form-label">Đánh giá</label>
                         <div class="col-sm-9">
-                            <input type="text" class="form-control" id="cono1" placeholder="Contact No Here">
+                            <input name="danh_gia" type="text" class="form-control" placeholder="Đánh giá">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="cono1" class="col-sm-3 text-right control-label col-form-label">Message</label>
+                        <label class="col-sm-3 text-right control-label col-form-label">Ngày
+                            chiếu</label>
                         <div class="col-sm-9">
-                            <textarea class="form-control"></textarea>
+                            <input name="ngay_chieu" type="text" class="form-control" placeholder="Ngày chiếu">
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-3 text-right control-label col-form-label">Trailer</label>
+                        <div class="col-sm-9">
+                            <input name="trailer" type="text" class="form-control" placeholder="Trailer">
                         </div>
                     </div>
                 </div>
                 <div class="border-top text-center">
                     <div class="card-body">
-                        <button type="button" class="btn btn-outline-success">Thêm</button>
+                        <button type="submit" class="btn btn-outline-success">Thêm</button>
                     </div>
                 </div>
             </form>
