ff9739673ead0186b744b3bd7a2cf6925e1831e8
doannvph33201 <118094808+doannvph33201@users.noreply.github.com>
2023-11-12 08:59:30 +0700

    1 chut ben admin, clone lai di 2 cu

--- admin/index.php
+++ admin/index.php
@@ -3,17 +3,19 @@ ob_start();
 $act = $_GET['act'] ?? "";
 
 switch ($act) {
-    case '':
-
+    case 'home':
+        $title = "Trang chủ";
+        $VIEW = "public/home.php";
         break;
 
     default:
-
+        $title = "Trang chủ";
+        $VIEW = "public/home.php";
         break;
 }
 ob_end_flush();
 
 include "layout/header.php";
 include "layout/left.php";
-include "phim/add.php";
+include  $VIEW;
 include "layout/footer.php";
